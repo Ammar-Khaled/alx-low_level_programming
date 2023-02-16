f46b44101650deb673500a20a79ac92f6c17dd2c
Ammar-Khaled <111376558+Ammar-Khaled@users.noreply.github.com>
2023-02-16 08:55:19 +0200

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 /**
- * main - prints exactly "and that piece of art is useful\" - Dora Korpar, 2015-10-19,"
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19,"
  * followed by a new line, to the standard error
  * Return: Always 1
  */
