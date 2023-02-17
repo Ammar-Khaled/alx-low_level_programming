df10b72094483d8aac4e381c6f953931661e0e95
Ammar-Khaled <111376558+Ammar-Khaled@users.noreply.github.com>
2023-02-17 08:52:15 +0200

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,18 @@
-#
+#include <stdio.h>
+
+/**
+ * main -  print alphabet
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char ch = 'a';
+while ( ch <= 'z' )
+  putchar(ch++);
+ch = 'A'
+while ( ch <= 'Z' )
+  putchar(ch++);
+putchar('\n');
+return (0);
+}
