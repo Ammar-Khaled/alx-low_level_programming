77f0e89a101dcf7e974db5d0db34fe93fd8597dc
Ammar-Khaled <111376558+Ammar-Khaled@users.noreply.github.com>
2023-02-17 09:02:05 +0200

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+/**
+ * main -  print alphabet
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char ch = 'a';
+while (ch <= 'z' && ch != 'q' && ch != 'e')
+{  
+  putchar(ch++);
+}
+putchar('\n');
+return (0);
+}
