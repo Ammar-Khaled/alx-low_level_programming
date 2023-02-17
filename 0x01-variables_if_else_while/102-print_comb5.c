aed0777b52fbd9557283fd1417eedd716bf651da
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-02-17 13:17:17 +0000

    OK

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,13 +12,10 @@ for (i1 = 0; i1 < 10; i1++)
 {
 	for (i2 = 0; i2 < 10; i2++)
 	{
-		putchar('0' + i1);
-		putchar('0' + i2);
-		putchar(' ');
 		if (i2 == 9)
 		{
 			j2 = 0;
-			j1++;
+			j1 = i1 + 1;
 		}
 		else
 		{
@@ -29,6 +26,9 @@ for (i1 = 0; i1 < 10; i1++)
 		{
 			for (; j2 < 10; j2++)
 			{
+				putchar('0' + i1);
+				putchar('0' + i2);
+				putchar(' ');
 				putchar('0' + j1);
 				putchar('0' + j2);		
 				if (i1 == 9 && i2 == 8 && j1 == 9 && j2 == 9)
