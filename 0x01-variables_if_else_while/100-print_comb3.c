ee60892aea78d869ba1fe55b9a197c9f653280f8
Ammar-Khaled <111376558+Ammar-Khaled@users.noreply.github.com>
2023-02-17 11:04:36 +0200

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,14 +12,17 @@ for (int i = 48; i < 58; i++)
 {
   for (int j = i+1; j < 58; j++)
   {
-   putchar(i); putchar(j); putchar(',');
-   if ( i == 56 && j == 57 )
-   {
-     putchar('\n');
-     break;
-   }
-   else
-     putchar(' ');
+	   putchar(i); putchar(j); 
+	   if ( i == 56 && j == 57 )
+	   {
+			putchar('\n');
+			break;
+	   }
+	   else
+	   {
+			putchar(',');
+			putchar(' ');
+	   }
   }
 }  
 return (0);
