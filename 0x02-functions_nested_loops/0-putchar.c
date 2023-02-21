eb2ba3b41e79b743d797180d1b8415db1ed06158
Ammar-Khaled <111376558+Ammar-Khaled@users.noreply.github.com>
2023-02-21 11:02:04 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * main -  prints "_putchar", followed by a new line.
- * 
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -14,5 +14,5 @@ while (i < 8)
 	i++;
 }
 _putchar('\n');
-return 0;
+return (0);
 }
