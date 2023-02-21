0516ac769fd05d1b8273a19cf96d514024fbad02
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-02-21 03:34:28 -0800

    OK

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,47 @@
 /**
+ * len - finds the length of a number
+ * @n: the number 
+ * Return: How many digits the number consists of
+ */
+int len(int n)
+{
+	int ans = 0;
+	while (n)
+	{
+		ans++;
+		n /= 10;
+	}
+	return n;
+}
+
+
+/**
  * times_table - prints the 9 times table, starting with 0.
  */
 void times_table(void)
 {
-
+int i, j, spaces, nn;
+for (i = 0; i < 10; i++)
+{
+	for (j = 0; j < 10; j++)
+	{
+		nn = i * j;
+		spaces = 3 - len(nn);
+		if (nn < 10)
+			_putchar(nn + '0');
+		else
+		{
+			_putchar((nn / 10) + '0');
+			_putchar((nn % 10) + '0');
+		}
+		if (j != 9)
+		{
+		_putchar(',');
+		while (spaces--)
+			_putchar(' ');
+		}
+		else
+			_putchar('\n');
+	}
+}
 }
