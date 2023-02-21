7221415f7432d936061e45a57c997f4e170d265e
Ammar-Khaled <111376558+Ammar-Khaled@users.noreply.github.com>
2023-02-21 11:49:49 +0200

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,31 @@
 #include "main.h"
+
+#include <unistd.h>
+/**
+ *
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1.  
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+return (write(1, &c, 1)); 
+}
+
+/**
+ * _abs - computes the absolute value of an integer.
+ * @n: the number to get its absolute
+ * Return: the absolute value of an integer.
+ */
+int _abs(int n)
+{
+if (n >= 0)
+	return (n);
+else
+	return (-1 * n);
+}
+
 /**
  * print_last_digit -  prints the last digit of a number.
  * @n: the number
@@ -8,7 +35,7 @@ int print_last_digit(int n)
 {
 int digit;
   
-digit = n % 10;
+digit = _abs(n) % 10;
 _putchar(digit);
-return digit;
+return (digit);
 }
