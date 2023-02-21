f12383f6100520392e4145728419ecbae2db909d
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-02-21 06:11:04 -0800

    OK

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,60 @@
+#include "main.h"
+/**
+ * len - finds the length of a number
+ * @n: the number
+ * Return: How many digits the number consists of
+ */
+int len(int n)
+{
+	int ans;
+
+	ans = 0;
+
+	if (n == 0)
+		return (1);
+
+	while (n)
+	{
+		ans++;
+		n /= 10;
+	}
+	return (ans);
+}
+
+
+/**
+ * times_table - prints the xth times table, starting with 0.
+ */
+void print_times_table(int x)
+{
+	if (n > 15 || n < 0)
+	{
+		_putchar('\n');
+		return;
+	}
+int i, j, spaces, n, nn;
+for (i = 0; i <= x; i++)
+{
+	for (j = 0; j <= x; j++)
+	{
+		n = i * j;
+		nn = i * (j + 1);
+		spaces = 4 - len(nn);
+		if (n < 10)
+			_putchar(n + '0');
+		else
+		{
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+		}
+		if (j != 9)
+		{
+		_putchar(',');
+		while (spaces--)
+			_putchar(' ');
+		}
+		else
+			_putchar('\n');
+	}
+}
+}
