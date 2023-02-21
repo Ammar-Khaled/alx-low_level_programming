1d50d4a550c5115fff24acbe7761d8a08ae46c12
Ammar-Khaled <111376558+Ammar-Khaled@users.noreply.github.com>
2023-02-21 18:39:10 +0200

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints the first 50 Fibonacci numbers, starting with 1 and 2, followed by a new line.
+ * main - prints the first 50 Fibonacci numbers,
+ * starting with 1 and 2, followed by a new line.
  * Return: Always 0
  */
 int main(void)
@@ -21,6 +22,5 @@ while (i < 51)
 		printf("\n");
 	i++;
 }
-printf("\n");
 return (0);
 }
