6c154ce3ae8a4910a7528e19b3a99b1b6bec6c72
Ammar-Khaled <111376558+Ammar-Khaled@users.noreply.github.com>
2023-02-21 18:57:14 +0200

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,15 +10,14 @@ long n1, n2, n, i;
 
 n1 = 1;
 n2 = 2;
-n = n1 + n2;
 printf("1, 2, ");
 i = 3;
 while (i < 99)
 {
+	n = n1 + n2;
 	printf("%ld", n);
 	n1 = n2;
 	n2 = n;
-	n = n1 + n2;
 	if (i != 98)
 		printf(", ");
 	else
