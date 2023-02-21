1692394428075e38cab323c2efaf9db98cd37b5d
Ammar-Khaled <111376558+Ammar-Khaled@users.noreply.github.com>
2023-02-21 12:16:06 +0200

    Update 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,3 +1,10 @@
+#include <stdio.h>
+
+/**
+ * sum-fib - considering the terms in the Fibonacci sequence
+ * whose values do not exceed 4,000,000, write a program that
+ * finds and prints the sum of the even-valued terms, followed by a new line.
+ */
 void sum_fib(void)
 {
 int n1 = 1, n2 = 2, n = n1 + n2;
