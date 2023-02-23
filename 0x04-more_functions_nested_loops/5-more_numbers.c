10bf6d235490506cf6c144f3cf5e3bf93988e9d5
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-02-23 10:08:33 +0000

    OK

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,13 +14,9 @@ void more_numbers(void)
 	{
 		for (j = 0; j < 15; j++)
 		{
-			if (j < 10)
-				_putchar(j + '0');
-			else
-			{
-				_putchar(1 + '0');
-				_putchar((j % 10) + '0');
-			}
+			if (j > 10)
+				_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
 		}
 		_putchar('\n');
 	}	
