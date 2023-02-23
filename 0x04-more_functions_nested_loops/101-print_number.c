4e0d0b9cc29fba2b2e1f68dea91b80b11934c1d8
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-02-23 11:02:01 -0800

    OK

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -17,13 +17,13 @@ void print_number(int n)
 	copy_n = n;
 	while (copy_n)
 	{
-		len++
+		len++;
 		copy_n /= 10;
 	}
 	
 	while (n > 9)
 	{
-		digit = n / (10 * (len - 1))
+		digit = n / (10 * (len - 1));
 		len--;
 		n /= 10;
 		_putchar(digit + '0');
