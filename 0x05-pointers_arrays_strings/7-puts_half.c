957b64f5900bf9a8aa513a5177c93be411246762
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-02-27 05:19:48 +0000

    OK

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,8 +1,38 @@
+#include "main.h"
 /**
- * puts_half - prints half of a string, followed by a new line.
+ * _strlen - computes the length of a string.
+ * @s: pointer to string.
+ * Return: How many characters the string contains.
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
+/**
+ * puts_half - prints the second half of a string,
+ * followed by a new line.
  * @str: pointer to the string
  */
 void puts_half(char *str)
 {
-	
+	int i, len;
+
+	len = _strlen(str);
+	i = len / 2;
+	if (len % 2 == 1)
+		i++;
+
+	for (; i < len; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
 }
