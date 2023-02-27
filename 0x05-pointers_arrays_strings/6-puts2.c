d5353918e33ef0e8e99331eecc23f7538698e247
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-02-27 05:11:38 +0000

    OK

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -17,7 +17,8 @@ int _strlen(char *s)
 	return (i);
 }
 /**
- * puts2 -  prints every other character of a string, starting with the first character,
+ * puts2 -  prints every other character of a string,
+ * starting with the first character,
  * followed by a new line.
  * @str: pointer to the string
  */
@@ -25,7 +26,7 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; i < _strlen(str); i+=2)
+	for (i = 0; i < _strlen(str); i += 2)
 	{
 		_putchar(str[i]);
 	}
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,8 @@
+/**
+ * puts_half - prints half of a string, followed by a new line.
+ * @str: pointer to the string
+ */
+void puts_half(char *str)
+{
+	
+}
