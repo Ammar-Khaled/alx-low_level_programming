f0e6f84133acd4349e96a5f16e8aa502fc653a27
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-02-27 04:36:32 +0000

    OK

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,10 @@
+/**
+ * _puts - prints a string, followed by a new line, to stdout.
+ * @str: pointer to string
+ * Return: void
+ */
+void _puts(char *str)
+{
+while (*str != '\0')
+    _putchar(*str);
+}
