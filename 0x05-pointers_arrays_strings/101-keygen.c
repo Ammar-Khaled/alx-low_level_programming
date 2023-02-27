d219c1a59d99333742a06d845c7c2329ac08669e
Ammar-Khaled <111376558+Ammar-Khaled@users.noreply.github.com>
2023-02-27 21:47:08 +0200

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,8 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - generates random password
+ * generates random ascii codes that add up to 2772 to crack the program
  * Return: Always 0
  */
 int main(void)
 {
-	return (2772);
+	int rand_num, sum;
+	
+	sum = 0;
+	srand(time(NULL) * getpid());
+	while (sum < 2772)
+	{
+		rand_num = rand() % 256;
+		if (sum + rand_num > 2772)
+		{
+			break;
+		}
+		sum += rand_num;
+		printf("%c", rand_num);
+	}
+	printf("%c\n", 2772 - sum);
+	return (0);
 }
