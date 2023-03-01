8091ed193f3f530e15ef40caee52fb57eb19f27e
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-03-01 15:16:39 +0000

    OK

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,6 +2,7 @@
  * _strncpy - copies n bytes from src to dest
  * @dest: destination string
  * @src: source string
+ * @n: number of characters to be copied
  * Return: pointer to the destination string
  */
 char *_strncpy(char *dest, char *src, int n)
@@ -11,7 +12,7 @@ char *_strncpy(char *dest, char *src, int n)
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 
-	for ( ; i < n; i++)
+	for (; i < n; i++)
 		dest[i] = '\0';
 
 	return (dest);
