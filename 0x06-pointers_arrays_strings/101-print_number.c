aa581de3b9991406be008cde27b8e09f5b39762e
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-03-02 19:00:21 +0000

    OK

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -5,14 +5,17 @@
  */
 void print_number(int n)
 {
-	if (n < 0)
+	unsigned int num;
+
+	num = n
+	if (num < 0)
 	{
 		_putchar('-');
-		n *= -1;
+		num *= -1;
 	}
 
-	if ((n / 10) > 0)
-		print_number(n / 10);
+	if ((num / 10) > 0)
+		print_number(num / 10);
 
-	_putchar((n % 10) + '0');
+	_putchar((num % 10) + '0');
 }
