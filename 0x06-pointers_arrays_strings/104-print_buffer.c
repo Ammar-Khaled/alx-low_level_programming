d31f709027edc1ee9e431ce46bdf4b8c2eeb9885
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-03-02 20:24:42 +0000

    OK

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,4 @@
+void print_buffer(char *b, int size)
+{
+	
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,3 +11,4 @@ char *rot13(char *);
 int is_separator(char ch);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_number(int n);
+void print_buffer(char *b, int size);
