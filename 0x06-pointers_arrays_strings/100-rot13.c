c07a716af6e81a6bfc70c7f976aa8099b2a1254d
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-03-02 18:44:40 +0000

    OK

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,21 +6,16 @@
  */
 char *rot13(char *s)
 {
-	int i, indx;
-	char alphabet[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-			   'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-			   'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-			   'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+	int i, j;
+	char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rotted_alphabet[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
+		for (j = 0; j < 52; j++)
 		{
-			for (indx = 0; indx < 52; indx++)
-			{
-				if (s[i] == alphabet[indx])
-					break;
-			}
-			s[i] = alphabet[(indx + 26) % 52];
+			if (s[i] == alphabet[j])
+				s[i] = rotted_alphabet[j];
 		}
 	}
 	return (s);
