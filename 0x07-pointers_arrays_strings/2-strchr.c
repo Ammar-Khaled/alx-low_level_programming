2515830586b802503a6d3beea25e030c154e1917
Ammar-Khaled <111376558+Ammar-Khaled@users.noreply.github.com>
2023-03-06 16:32:45 +0200

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,11 +9,12 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
-	{
-		if (*s == c)
-			return s;
-		s++;
-	}
-	return (NULL);
+	while(*s != c && *s != '\0') {
+	      s++;
+	   }
+   if(*s == c) {
+      return s;
+   }else {
+      return NULL;
+   }
 }
