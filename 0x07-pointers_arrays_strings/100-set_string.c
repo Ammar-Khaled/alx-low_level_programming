0ea8b1e4d3429989ec7cc27c1e1cc9c045c32c20
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-03-06 15:01:49 +0000

    initial commit

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,1 +1,23 @@
-void set_string(char **s, char *to);
+
+/**
+ * set_string - sets the value of a pointer to a char.
+ * @s: pointer to the pointer to the string to be set
+ * @to: pointer to the string to set
+ */
+void set_string(char **s, char *to)
+{
+	while (**s != '\0')
+	{
+		if (*to != '\0')
+		{
+			**s = *to;
+			to++;
+			(*s)++;
+		}
+		else
+		{
+			**s = '\0';
+			return;
+		}
+	}
+}
