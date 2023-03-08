b1751b606837459f7ea313ff412b69b16089347f
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-03-08 15:40:18 +0000

    initial commit

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,2 +1,23 @@
+/**
+ * wildcmp - compares two strings including the wildcard "*"
+ * @s1: pointer to the first string
+ * @s2: pointer to the second string that can contain wildcard *
+ * which can replace any string (including an empty string)
+ * Return:  1 if the strings can be considered identical, otherwise return 0.
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
 
-int wildcmp(char *s1, char *s2);
+	if (*s1 == '\0' && *s2 == '*' && *(s2 + 1) != '\0')
+		return (0);
+
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || wildcard(s1 + 1, s2));
+
+	return (0);
+}
