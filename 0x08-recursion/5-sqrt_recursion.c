eabd075d4e1268f7bdfb64e26c6aed41c17a8196
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-03-08 16:05:12 +0000

    initial commit

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,8 +1,15 @@
-
+/**
+ * try - find the square of numbers from 2 to N,
+ *  till the square of some number K becomes greater than or equal N.
+ *  Hence the value of K will be the floor square root of N.
+ * @n: number we mant to find its sqr
+ * @k: cuurent test number
+ * Return: the floor integer value of square root of n;
+ */
 int try(int n, int k)
 {
 	if (k * k >= n)
-		return k;
+		return (k);
 
 	return (try(n, k + 1));
 }
