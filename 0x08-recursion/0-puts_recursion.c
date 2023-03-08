17942bc5dd5b50bffe573567cb1741861ed3eefa
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-03-08 04:20:03 +0000

    initial commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: pointer to the string
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
