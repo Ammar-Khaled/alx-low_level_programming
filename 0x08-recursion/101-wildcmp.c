b08bcf8361c167a683c93e446c1af05ef4bd1194
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-03-08 15:42:20 +0000

    initial commit

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -17,7 +17,7 @@ int wildcmp(char *s1, char *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 
 	if (*s2 == '*')
-		return (wildcmp(s1, s2 + 1) || wildcard(s1 + 1, s2));
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 
 	return (0);
 }
