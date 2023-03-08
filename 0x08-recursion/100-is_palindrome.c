70570feb9b1f28ae7d75998dfead896de6828695
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-03-08 04:05:13 +0000

    initial commit

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -11,5 +11,5 @@ int is_palindrome(char *s, int len)
 {
 	if (len <= 1)
 		return (1);
-	return (s[0] == s[len - 1] && is_palindrome(s + 1, len - 2));
+	return (int) (s[0] == s[len - 1] && is_palindrome(s + 1, len - 2));
 }
