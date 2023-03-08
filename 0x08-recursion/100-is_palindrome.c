b434ea428eb1a33a3503baad8001394b691f70d6
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-03-08 04:03:04 +0000

    initial commit

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,9 +1,15 @@
 #include <string.h>
 
-
+/**
+ * is_palindrome - checks whether the array could be read from both ends
+ * the same way or not
+ * @s: the string
+ * @len: the length of the string
+ * @Return: 1 if a string is a palindrome and 0 if not.
+ */
 int is_palindrome(char *s, int len)
 {
 	if (len <= 1)
-		return 1;
-	return s[0] == s[len - 1] && is_palindrome(s + 1, len - 2);
+		return (1);
+	return (s[0] == s[len - 1] && is_palindrome(s + 1, len - 2));
 }
