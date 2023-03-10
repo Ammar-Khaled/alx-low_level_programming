a99d70840d693eff19e68ffc111e70e044ecbd21
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-03-10 09:32:19 +0000

    initial commit

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -16,44 +16,34 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-
 	cents = atoi(argv[1]);
 	if (cents < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
-
 	coins = 0;
 	while (cents >= 25)
 	{
 		coins++;
 		cents -= 25;
 	}
-
 	while (cents >= 10)
 	{
 		coins++;
 		cents -= 10;
 	}
-
 	while (cents >= 5)
 	{
 		coins++;
 		cents -= 5;
 	}
-
 	while (cents >= 2)
 	{
 		coins++;
 		cents -= 2;
 	}
-
-	while (cents >= 1)
-	{
-		coins++;
-		cents--;
-	}
+	coins += cents;
 	printf("%d\n", coins);
 	return (0);
 }
