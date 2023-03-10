a32efb88e5fdc17db68f2ecbacaeba2ab0b0985f
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-03-10 09:22:58 +0000

    initial commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,8 +2,9 @@
 /**
  * main - prints the name of the program
 */
-int main(char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	if (argc == 1)
+		printf("%s\n", argv[0]);
 	return (0);
 }
