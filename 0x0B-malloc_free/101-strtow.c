72f6dcb1dbbbc040e28ed3420192ab63af1dc0ba
Ammar-Khaled <111376558+Ammar-Khaled@users.noreply.github.com>
2023-03-13 13:05:37 +0200

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,6 +1,4 @@
 #include <stdlib.h>
-#include <limits.h>
-
 /**
  * strtow - splits a string into words.
  * @str: pointer to the string to be splited
@@ -14,42 +12,39 @@ char **strtow(char *str)
 
 	if (str == NULL || *str == '\0')
 		return (NULL);
-
-	/* count words*/
-	count = 0;
+	count = 0; /* count words*/
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
 			count++;
 	}
-
 	if (count == 0)
 		return (NULL);
-
 	p = malloc((count + 1) * sizeof(char *));
 	if (p == NULL)
 		return (NULL);
-
 	count = j = index = 0;
-
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] != ' ')
-		{
 			count++;
-		}
 		else if (count != 0)
 		{
-			p[index] = malloc((sizeof(char) * count) + 1);
+			p[index] = malloc(sizeof(char) * (count + 1));
 			for (j = 0, k = i - count; k < i; j++, k++)
-			{
 				p[index][j] = str[k];
-			}
 			p[index][j] = '\0';
 			index++;
 			count = j = 0;
 		}
 	}
-	p[index] = NULL;
+	if (count != 0)
+	{
+		p[index] = malloc(sizeof(char) * (count + 1));
+		for (j = 0, k = i - count; k < i; j++, k++)
+			p[index][j] = str[k];
+		p[index][j] = '\0';
+	}
+	p[++index] = NULL;
 	return (p);
 }
