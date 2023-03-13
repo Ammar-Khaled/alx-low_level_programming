0ee66c9f16996b557d2f9f64bfe322c915f5b4d2
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-03-13 07:13:22 +0000

    null handling

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -33,13 +33,15 @@ char *str_concat(char *s1, char *s2)
 	if (p == NULL)
 		return (NULL);
 
-	for (i = 0; s1[i] != '\0'; i++)
-		p[i] = s1[i];
+	i = 0;
+	if (s1 != NULL)
+		for (; s1[i] != '\0'; i++)
+			p[i] = s1[i];
 
-	for (j = 0; s2[j] != '\0'; j++, i++)
-	{
-		p[i] = s2[j];
-	}
+	if (s2 != NULL)
+		for (j = 0; s2[j] != '\0'; j++, i++)
+			p[i] = s2[j];
+
 
 	p[i] = '\0';
 	return (p);
