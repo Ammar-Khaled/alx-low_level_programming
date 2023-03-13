c0f71c448a0d9207f9d0d4bc8bcb7cc8cbf2bc38
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-03-13 07:50:13 +0000

    quick fix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -7,18 +7,18 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int **p, i, j, k;
+	int **p, i, j;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	p = malloc(width * sizeof(int *));
+	p = malloc(height * sizeof(int *));
 	if (p == NULL)
 		return (NULL);
 
-	for (i = 0; i < width; i++)
+	for (i = 0; i < height; i++)
 	{
-		p[i] = malloc(height * sizeof(int));
+		p[i] = malloc(width * sizeof(int));
 		if (p[i] == NULL)
 		{
 			while (i--)
@@ -28,8 +28,8 @@ int **alloc_grid(int width, int height)
 		}
 	}
 
-	for (i = 0; i < width; i++)
-		for (j = 0; j < height; j++)
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
 			p[i][j] = 0;
 
 	return (p);
