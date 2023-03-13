13420dd253f415ba200dd8e699727c923fe1a2b2
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-03-13 09:51:16 +0000

    Hot fix

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -18,9 +18,12 @@ char **strtow(char *str)
 	count = 0;
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] != ' ' && str[i + 1] == ' ')
+		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
 			count++;
 	}
+
+	if (count == 0)
+		return (NULL);
 
 	p = malloc((count * sizeof(char *)) + 1);
 	if (p == NULL)
