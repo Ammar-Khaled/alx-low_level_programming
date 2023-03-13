411324e051de67095c62fc54b7c0c9ad0ccb3bf2
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-03-13 05:45:24 +0000

    fix bugs

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,15 +9,18 @@
  */
 char *create_array(unsigned int size, char c)
 {
+	unsigned int i;
+	char *p;
+
 	if (size == 0)
 		return (NULL);
 
-	char *p = malloc(size * sizeof(char));
+	p = malloc(size * sizeof(char));
 	if (p == NULL)
 		return (NULL);
 
-	while (size--)
-		*p++ == c;
-	
+	for (i = 0; i < size; i++)
+		p[i] = c;
+
 	return (p);
 }
