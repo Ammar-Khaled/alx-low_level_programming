a0f18564ba1f396020080af44bb1817a470aa354
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-03-16 12:15:16 +0000

    fix coding style

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -8,11 +8,16 @@
 */
 int min(int a, int b)
 {
-	return (a < b) ? (a) : (b);
+	return ((a < b) ? (a) : (b));
 }
 
 /**
  * _realloc - reallocates a memory block using malloc and free
+ * @ptr:  a pointer to the memory previously allocated with a call to malloc:
+ * malloc(old_size).
+ * @old_size: is the size, in bytes, of the allocated space for ptr.
+ * @new_size: is the new size, in bytes of the new memory block.
+ * Return: a pointer to the newly reallocated memory block
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
