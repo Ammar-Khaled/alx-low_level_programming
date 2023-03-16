0524a89c51e6ca2e3a2f5323bfd9e7bed1363513
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-03-16 11:56:54 +0000

    initial commit

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,9 +1,28 @@
 #include <stdlib.h>
-
+#include <string.h>
+#include <math.h>
 /**
  * _realloc - reallocates a memory block using malloc and free
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	
+	void *tmp;
+	int i;
+
+	if (ptr == NULL)
+		return (malloc(new_size));
+
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (old_size == new_size)
+		return (ptr);
+
+	tmp = malloc(new_size);
+	strncpy(tmp, ptr, min(old_size, new_size));
+	free(ptr);
+	return (tmp);
 }
