3c75c6eb532850775f5df1a7ca3ba0f5441bc27e
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-03-20 14:46:06 +0000

    fix displaying format

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,12 +16,12 @@ void print_dog(struct dog *d)
 		printf("Name: (nil)\n");
 
 	if (d->age)
-		printf("Name: %f\n", d->age);
+		printf("Age: %f\n", d->age);
 	else
-		printf("Name: (nil)\n");
+		printf("Age: (nil)\n");
 
 	if (d->owner)
-		printf("Name: %s\n", d->owner);
+		printf("Owner: %s\n", d->owner);
 	else
-		printf("Name: (nil)\n");
+		printf("Owner: (nil)\n");
 }
