d2999f8b3c81d81b1f6be52a6315a32033582e67
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-03-20 15:09:30 +0000

    handling NULL arguments

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -5,38 +5,39 @@
  * new_dog - creates a new dog.
  * @name: name for the new dog
  * @age: age for the new dog
- * @omner: omner for the new dog
+ * @owner: omner for the new dog
  *
  * Return: pointer to the new dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	char *name_cp;
-	char *owner_cp;
 	dog_t *d;
 
 	d = malloc(sizeof(dog_t));
 	if (!d || !name || !owner)
-		return (NULL);
-
-	name_cp = malloc(strlen(name) + 1);
-	if (!name_cp || !(name_cp = strcpy(name_cp, name)))
 	{
 		free(d);
 		return (NULL);
 	}
 
-	owner_cp = malloc(strlen(owner) + 1);
-	if (!owner_cp || !(owner_cp = strcpy(owner_cp, owner)))
+	d->name = malloc(strlen(name) + 1);
+	d->name = strcpy(d->name, name);
+	if (!d->name)
 	{
 		free(d);
-		free(name_cp);
 		return (NULL);
 	}
 
-	d->name = name_cp;
+	d->owner = malloc(strlen(owner) + 1);
+	d->owner = strcpy(d->owner, owner);
+	if (!d->owner)
+	{
+		free(d);
+		free(d->name);
+		return (NULL);
+	}
+
 	d->age = age;
-	d->owner = owner;
 
 	return (d);
 }
