ef2caf4b6ea4b8ae20dc1e77cba93c30074f90bd
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-03-20 14:52:49 +0000

    handling age of 0

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,10 +15,8 @@ void print_dog(struct dog *d)
 	else
 		printf("Name: (nil)\n");
 
-	if (d->age)
-		printf("Age: %f\n", d->age);
-	else
-		printf("Age: (nil)\n");
+
+	printf("Age: %f\n", d->age);
 
 	if (d->owner)
 		printf("Owner: %s\n", d->owner);
