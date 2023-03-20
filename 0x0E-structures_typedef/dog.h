1a48f39c2b02a92b3110a274e3ab09ef03594351
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-03-20 14:34:23 +0000

    fix style

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,11 @@
+#ifndef DOG_H
+#define DOG_H
 /**
- * dog - represents a dog structure
+ * struct dog - represents a dog structure
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
+ *
  */
 struct dog
 {
@@ -13,10 +16,11 @@ struct dog
 
 /**
  * dog_t - Typedef for struct dog
-*/
+ */
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+#endif
