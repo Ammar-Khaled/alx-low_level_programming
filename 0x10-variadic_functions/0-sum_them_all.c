5f5aea16e81c954a0e78996f085b669221508cda
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-03-23 15:16:08 +0000

    quick fixes

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -15,11 +15,11 @@ int sum_them_all(const unsigned int n, ...)
 		return (0);
 
 	va_start(args, n);
-	
+
 	sum = 0;
 	for (i = 0; i < n; i++)
 		sum += va_arg(args, int);
-	
+
 	va_end(args);
 
 	return (sum);
--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,7 +13,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%i", va_arg(args, int));
+		printf("%d", va_arg(args, int));
 		if (i == n - 1)
 			printf("\n");
 		else if (separator)
