9618bcd585917bb0de9feb366cf1ce6e8d464bb4
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-04-04 06:26:04 +0000

    initial commit

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,51 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * length - finds the length of the list from `first` to `last`
+ * @first: pointer to the first node
+ * @last: pointer to the last node
+ *
+ * Return: the number of nodes between first and last
+ */
+size_t length(const listint_t *first, const listint_t *last)
+{
+	size_t count = 0;
+	const listint_t *current = first;
+
+	while (current != last)
+	{
+		current = current->next;
+		count++;
+	}
+
+	return (count + 1);
+}
+
+/**
+ * print_listint_safe - prints a listint_t linked list with a loop
+ * @head: pointer to the list
+ *
+ * Return: the number of nodes in the list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t count = 0, previous_length = 0;
+	const listint_t *first, *last;
+
+	first = last = head;
+	while (last && length(first, last) > previous_length)
+	{
+		previous_length = length(first, last);
+
+		printf("[%p] %i\n", (void *)last, last->n);
+		count++;
+		last = last->next;
+	}
+
+	if (last)
+	{
+		printf("-> [%p] %i\n", (void *)last, last->n);
+	}
+
+	return (count);
+}
