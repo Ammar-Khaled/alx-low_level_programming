8b69cfba2ebc86e2b060a9c180c2157494469de1
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-04-06 13:36:47 +0000

    initial commit

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: file name
+ * @letters: the number of letters to read and print
+ *
+ * Return: the actual number of letters it could read and print, or 0 otherwise
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buf;
+	ssize_t read_count, write_count;
+
+	if (!filename)
+		return (0);
+	
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+		return (0);
+		
+	read_count = read(fd, buf, letters);
+	write_count = write(STDOUT_FILENO, buf, read_count);
+
+
+	close(fd);
+	free(buf);
+	return (write_count);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <sys/types.h>	/* for ssize_t */
+#include <sys/stat.h>
+#include <fcntl.h> 	/* for open() and O_RDWR */
+#include <unistd.h> 	/* for read(), write(), and close() */
+#include <stdlib.h>	/* for size_t */
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif
