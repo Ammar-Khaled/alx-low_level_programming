3a5253990aa7d29281bdd5ef67a3c16916a6caf8
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-05-18 18:14:25 +0000

    initial commit

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of a
+ * dlistint_t list
+ *
+ * @h: head of the list
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+    const dlistint_t *current = h;
+    size_t count = 0;
+
+    while (current)
+    {
+        printf("%i\n", current->n);
+        count++;
+        current = current->next;
+    }
+
+    return (count);
+}
