0acfc8f4e7249696b89dbf3a1713e0939da7a44d
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-05-18 20:44:04 +0000

    fix style

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -39,7 +39,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	head = *h;
 	i = 1;
-	
+
 	if (head)
 	{
 		while (head->prev)
@@ -50,7 +50,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			if (i == idx)
 			{
 				if (!head->next)
-					return (add_dnodeint_end(h, n));
+					new = add_dnodeint_end(h, n);
 				else
 				{
 					new = malloc(sizeof(dlistint_t));
