0f684ba213c60a2523f2732e4d0f50c52d963be3
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-05-18 20:29:04 +0000

    consider a test case

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -36,6 +36,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
+	if (idx == dlistint_len(*h)- 1)
+		return (add_dnodeint_end(h, n));
+
 	if (idx > dlistint_len(*h))
 		return (NULL);
 
