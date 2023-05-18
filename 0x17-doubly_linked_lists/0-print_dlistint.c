3728ef6bcaffcdcf3e237df8d16e65f31dbdbc2e
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-05-18 19:53:22 +0000

    fix style

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,15 +9,15 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    const dlistint_t *current = h;
-    size_t count = 0;
+	const dlistint_t *current = h;
+	size_t count = 0;
 
-    while (current)
-    {
-        printf("%i\n", current->n);
-        count++;
-        current = current->next;
-    }
+	while (current)
+	{
+		printf("%i\n", current->n);
+		count++;
+		current = current->next;
+	}
 
-    return (count);
+	return (count);
 }
