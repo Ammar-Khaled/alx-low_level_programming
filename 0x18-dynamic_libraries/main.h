f4e2d10a8b689de302bb9a79eb19e68f1a9659e7
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-05-22 10:01:40 +0000

    add include guard

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,3 +1,6 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -19,3 +22,5 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 int accepted(char c, char *accept);
+
+#endif
