6ca805e83d2637dd6657a0ec12cb0739f3fc0cda
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-07-26 12:24:28 +0000

    Complete technical interview

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+def check_neighbours(grid, i, j, perimeter):
+    """Checks the land cells adjacent to a specific cell."""
+    subtract = 0
+
+    if i > 0 and grid[i - 1][j] == 1:
+        subtract += 1
+    if j > 0 and grid[i][j - 1] == 1:
+        subtract += 1
+    if i < len(grid) - 1 and grid[i + 1][j] == 1:
+        subtract += 1
+    if j < len(grid[i]) - 1 and grid[i][j + 1] == 1:
+        subtract += 1
+
+    return subtract
+
+
+def island_perimeter(grid):
+    """Returns the perimeter of the island represented by the grid."""
+    perimeter = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                perimeter += 4
+                perimeter -= check_neighbours(grid, i, j)
+
+    return perimeter
