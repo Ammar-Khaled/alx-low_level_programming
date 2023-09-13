45aaa8ed6af2f68678fc964cd2d5151b2dba8efd
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-09-13 08:09:26 +0000

    Updated the main header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -23,14 +23,37 @@ int advanced_binary(int *array, size_t size, int value);
  */
 typedef struct listint_s
 {
-    int n;
-    size_t index;
-    struct listint_s *next;
+	int n;
+	size_t index;
+	struct listint_s *next;
 } listint_t;
 listint_t *jump_list(listint_t *list, size_t size, int value);
+
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ */
+typedef struct skiplist_s
+{
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
+} skiplist_t;
 
 void free_list(listint_t *list);
 listint_t *create_list(int *array, size_t size);
 void print_list(const listint_t *list);
+
+void free_skiplist(skiplist_t *list);
+void init_express(skiplist_t *list, size_t size);
+skiplist_t *create_skiplist(int *array, size_t size);
+void print_skiplist(const skiplist_t *list);
 
 #endif
